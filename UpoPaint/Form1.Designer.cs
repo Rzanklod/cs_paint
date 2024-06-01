f60354e6016ed1a4e9dbb7716262f32efac74559
Rzanklod <malwinaxxx666@gmail.com>
2024-06-01 17:11:32 +0200

    Add project files.

---
+++ UpoPaint/Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿
+namespace UpoPaint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.radioButtonRectangle = new System.Windows.Forms.RadioButton();
+            this.radioButtonCircle = new System.Windows.Forms.RadioButton();
+            this.radioButtonLine = new System.Windows.Forms.RadioButton();
+            this.radioButtonCurve = new System.Windows.Forms.RadioButton();
+            this.pictureBoxPaint = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.plikToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nowyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.otworzToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zapiszToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonFillColor = new System.Windows.Forms.Button();
+            this.checkBoxFill = new System.Windows.Forms.CheckBox();
+            this.buttonPenColor = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.cofnijToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ponowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPaint)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox3);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox2);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBoxPaint);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 426);
+            this.splitContainer1.SplitterDistance = 151;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButtonCurve);
+            this.groupBox1.Controls.Add(this.radioButtonLine);
+            this.groupBox1.Controls.Add(this.radioButtonCircle);
+            this.groupBox1.Controls.Add(this.radioButtonRectangle);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(136, 119);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Typ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.numericUpDown1);
+            this.groupBox2.Controls.Add(this.buttonPenColor);
+            this.groupBox2.Location = new System.Drawing.Point(12, 137);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(136, 100);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Pedzel";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.checkBoxFill);
+            this.groupBox3.Controls.Add(this.buttonFillColor);
+            this.groupBox3.Location = new System.Drawing.Point(12, 243);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(136, 100);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Wypelnienie";
+            // 
+            // radioButtonRectangle
+            // 
+            this.radioButtonRectangle.AutoSize = true;
+            this.radioButtonRectangle.Location = new System.Drawing.Point(6, 65);
+            this.radioButtonRectangle.Name = "radioButtonRectangle";
+            this.radioButtonRectangle.Size = new System.Drawing.Size(64, 17);
+            this.radioButtonRectangle.TabIndex = 0;
+            this.radioButtonRectangle.Text = "Kwadrat";
+            this.radioButtonRectangle.UseVisualStyleBackColor = true;
+            this.radioButtonRectangle.Click += new System.EventHandler(this.radioButtonRectangle_Click);
+            // 
+            // radioButtonCircle
+            // 
+            this.radioButtonCircle.AutoSize = true;
+            this.radioButtonCircle.Location = new System.Drawing.Point(6, 42);
+            this.radioButtonCircle.Name = "radioButtonCircle";
+            this.radioButtonCircle.Size = new System.Drawing.Size(46, 17);
+            this.radioButtonCircle.TabIndex = 1;
+            this.radioButtonCircle.Text = "Kolo";
+            this.radioButtonCircle.UseVisualStyleBackColor = true;
+            this.radioButtonCircle.Click += new System.EventHandler(this.radioButtonCircle_Click);
+            // 
+            // radioButtonLine
+            // 
+            this.radioButtonLine.AutoSize = true;
+            this.radioButtonLine.Checked = true;
+            this.radioButtonLine.Location = new System.Drawing.Point(6, 19);
+            this.radioButtonLine.Name = "radioButtonLine";
+            this.radioButtonLine.Size = new System.Drawing.Size(55, 17);
+            this.radioButtonLine.TabIndex = 2;
+            this.radioButtonLine.TabStop = true;
+            this.radioButtonLine.Text = "Prosta";
+            this.radioButtonLine.UseVisualStyleBackColor = true;
+            this.radioButtonLine.Click += new System.EventHandler(this.radioButtonLine_Click);
+            // 
+            // radioButtonCurve
+            // 
+            this.radioButtonCurve.AutoSize = true;
+            this.radioButtonCurve.Location = new System.Drawing.Point(6, 88);
+            this.radioButtonCurve.Name = "radioButtonCurve";
+            this.radioButtonCurve.Size = new System.Drawing.Size(59, 17);
+            this.radioButtonCurve.TabIndex = 3;
+            this.radioButtonCurve.Text = "Krzywa";
+            this.radioButtonCurve.UseVisualStyleBackColor = true;
+            this.radioButtonCurve.Click += new System.EventHandler(this.radioButtonCurve_Click);
+            // 
+            // pictureBoxPaint
+            // 
+            this.pictureBoxPaint.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxPaint.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxPaint.Name = "pictureBoxPaint";
+            this.pictureBoxPaint.Size = new System.Drawing.Size(645, 426);
+            this.pictureBoxPaint.TabIndex = 0;
+            this.pictureBoxPaint.TabStop = false;
+            this.pictureBoxPaint.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBoxPaint.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBoxPaint_MouseMove);
+            this.pictureBoxPaint.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.plikToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // plikToolStripMenuItem
+            // 
+            this.plikToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nowyToolStripMenuItem,
+            this.otworzToolStripMenuItem,
+            this.zapiszToolStripMenuItem,
+            this.cofnijToolStripMenuItem,
+            this.ponowToolStripMenuItem});
+            this.plikToolStripMenuItem.Name = "plikToolStripMenuItem";
+            this.plikToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.plikToolStripMenuItem.Text = "Plik";
+            // 
+            // nowyToolStripMenuItem
+            // 
+            this.nowyToolStripMenuItem.Name = "nowyToolStripMenuItem";
+            this.nowyToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.nowyToolStripMenuItem.Text = "Nowy";
+            this.nowyToolStripMenuItem.Click += new System.EventHandler(this.nowyToolStripMenuItem_Click);
+            // 
+            // otworzToolStripMenuItem
+            // 
+            this.otworzToolStripMenuItem.Name = "otworzToolStripMenuItem";
+            this.otworzToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.otworzToolStripMenuItem.Text = "Otworz";
+            this.otworzToolStripMenuItem.Click += new System.EventHandler(this.otworzToolStripMenuItem_Click);
+            // 
+            // zapiszToolStripMenuItem
+            // 
+            this.zapiszToolStripMenuItem.Name = "zapiszToolStripMenuItem";
+            this.zapiszToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.zapiszToolStripMenuItem.Text = "Zapisz";
+            this.zapiszToolStripMenuItem.Click += new System.EventHandler(this.zapiszToolStripMenuItem_Click);
+            // 
+            // buttonFillColor
+            // 
+            this.buttonFillColor.Location = new System.Drawing.Point(6, 19);
+            this.buttonFillColor.Name = "buttonFillColor";
+            this.buttonFillColor.Size = new System.Drawing.Size(83, 27);
+            this.buttonFillColor.TabIndex = 0;
+            this.buttonFillColor.UseVisualStyleBackColor = true;
+            this.buttonFillColor.Click += new System.EventHandler(this.buttonFillColor_Click);
+            // 
+            // checkBoxFill
+            // 
+            this.checkBoxFill.AutoSize = true;
+            this.checkBoxFill.Location = new System.Drawing.Point(6, 52);
+            this.checkBoxFill.Name = "checkBoxFill";
+            this.checkBoxFill.Size = new System.Drawing.Size(70, 17);
+            this.checkBoxFill.TabIndex = 2;
+            this.checkBoxFill.Text = "Tak / nie";
+            this.checkBoxFill.UseVisualStyleBackColor = true;
+            // 
+            // buttonPenColor
+            // 
+            this.buttonPenColor.Location = new System.Drawing.Point(11, 19);
+            this.buttonPenColor.Name = "buttonPenColor";
+            this.buttonPenColor.Size = new System.Drawing.Size(65, 34);
+            this.buttonPenColor.TabIndex = 0;
+            this.buttonPenColor.UseVisualStyleBackColor = true;
+            this.buttonPenColor.Click += new System.EventHandler(this.buttonPenColor_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(16, 59);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            40,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(60, 20);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // cofnijToolStripMenuItem
+            // 
+            this.cofnijToolStripMenuItem.Name = "cofnijToolStripMenuItem";
+            this.cofnijToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Z)));
+            this.cofnijToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cofnijToolStripMenuItem.Text = "Cofnij";
+            this.cofnijToolStripMenuItem.Click += new System.EventHandler(this.cofnijToolStripMenuItem_Click);
+            // 
+            // ponowToolStripMenuItem
+            // 
+            this.ponowToolStripMenuItem.Name = "ponowToolStripMenuItem";
+            this.ponowToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ponowToolStripMenuItem.Text = "Ponow";
+            this.ponowToolStripMenuItem.Click += new System.EventHandler(this.ponowToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPaint)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButtonCurve;
+        private System.Windows.Forms.RadioButton radioButtonLine;
+        private System.Windows.Forms.RadioButton radioButtonCircle;
+        private System.Windows.Forms.RadioButton radioButtonRectangle;
+        private System.Windows.Forms.PictureBox pictureBoxPaint;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem plikToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nowyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem otworzToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zapiszToolStripMenuItem;
+        private System.Windows.Forms.Button buttonFillColor;
+        private System.Windows.Forms.CheckBox checkBoxFill;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button buttonPenColor;
+        private System.Windows.Forms.ToolStripMenuItem cofnijToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ponowToolStripMenuItem;
+    }
+}
+
---
+++ UpoPaint/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UpoPaint
+{
+    public partial class Form1 : Form
+    {
+        private Graphics graphics;
+        private List<Point> points = new List<Point>();
+        private List<Image> history;
+        private int selectedShape = 0;
+        private int undoCount = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            nowyToolStripMenuItem_Click(null, null);
+            buttonPenColor.BackColor = Color.Black;
+        }
+
+        private void addHistory()
+        {
+            Bitmap b = new Bitmap(pictureBoxPaint.Image);
+            if(history.Count > 19)
+            {
+                history.Remove(history.First());
+            }
+            history.Add(b);
+        }
+        private void nowyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBoxPaint.Image = new Bitmap(pictureBoxPaint.Width, pictureBoxPaint.Height);
+            graphics = Graphics.FromImage(pictureBoxPaint.Image);
+            graphics.Clear(Color.White);
+            history = new List<Image>();
+            undoCount = 0;
+            addHistory();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(e.Button != MouseButtons.Left) return;
+
+            points = new List<Point>();
+            points.Add(e.Location);
+
+            while (undoCount > 0)
+            {
+                history.Remove(history.Last());
+                undoCount--;
+            }
+        }
+
+        private void pictureBoxPaint_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left) return;
+
+            points.Add(e.Location);
+
+            pictureBoxPaint.Image = new Bitmap(history.Last());
+            graphics = Graphics.FromImage(pictureBoxPaint.Image);
+            Pen p = new Pen(buttonPenColor.BackColor, (int)numericUpDown1.Value);
+            Brush b = new SolidBrush(buttonFillColor.BackColor);
+            draw(p, b);
+            pictureBoxPaint.Refresh();
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left) return;
+
+            points.Add(e.Location);
+
+            Pen p = new Pen(buttonPenColor.BackColor, (int)numericUpDown1.Value);
+            Brush b = new SolidBrush(buttonFillColor.BackColor);
+            draw(p, b);
+            addHistory();
+            pictureBoxPaint.Refresh();
+        }
+
+        private void draw(Pen p, Brush b)
+        {
+            int x = Math.Min(points.First().X, points.Last().X);
+            int y = Math.Min(points.First().Y, points.Last().Y);
+            int width = Math.Abs(points.First().X - points.Last().X);
+            int height = Math.Abs(points.First().Y - points.Last().Y);
+            bool fillChecked = checkBoxFill.Checked;
+
+            switch (selectedShape)
+            {
+                case 0:
+                    graphics.DrawLine(p, points.First(), points.Last());
+                    break;
+
+                case 1:
+                    if (fillChecked) graphics.FillEllipse(b, x, y, width, height);
+                    graphics.DrawEllipse(p, x, y, width, height);
+                    break;
+
+                case 2:
+                    if (fillChecked) graphics.FillRectangle(b, x, y, width, height);
+                    graphics.DrawRectangle(p, x, y, width, height);
+                    break;
+
+                case 3:
+                    graphics.DrawCurve(p, points.ToArray());
+                    break;
+            }
+        }
+
+        private void radioButtonLine_Click(object sender, EventArgs e)
+        {
+            selectedShape = 0;
+        }
+
+        private void radioButtonCircle_Click(object sender, EventArgs e)
+        {
+            selectedShape = 1;
+        }
+
+        private void radioButtonRectangle_Click(object sender, EventArgs e)
+        {
+            selectedShape = 2;
+        }
+
+        private void radioButtonCurve_Click(object sender, EventArgs e)
+        {
+            selectedShape = 3;
+        }
+
+        private void buttonFillColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            cd.Color = buttonFillColor.BackColor;
+            if (cd.ShowDialog() != DialogResult.OK) return;
+            
+            buttonFillColor.BackColor = cd.Color;
+            
+        }
+
+        private void buttonPenColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            cd.Color = buttonPenColor.BackColor;
+            
+            if (cd.ShowDialog() != DialogResult.OK) return;
+            
+            buttonPenColor.BackColor = cd.Color;
+            
+        }
+
+        private void zapiszToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "JPEG|*.jpg|Bitmapa|*.bmp";
+
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+
+            string extension = Path.GetExtension(sfd.FileName);
+
+            switch (extension)
+            {
+                case ".jpg":
+                    pictureBoxPaint.Image.Save(sfd.FileName, ImageFormat.Jpeg);  
+                    break;
+
+                case ".bmp":
+                    pictureBoxPaint.Image.Save(sfd.FileName, ImageFormat.Bmp);
+                    break;
+            }
+        }
+
+        private void otworzToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "JPEG|*.jpg|Bitmapa|*.bmp";
+
+            if (ofd.ShowDialog() != DialogResult.OK) return;
+
+            pictureBoxPaint.Image = new Bitmap(ofd.FileName);
+            graphics = Graphics.FromImage(pictureBoxPaint.Image);
+            history = new List<Image>();
+            addHistory();
+            undoCount = 0;
+        }
+
+        private void cofnijToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (undoCount >= history.Count - 1) return;
+           
+            undoCount++;
+            pictureBoxPaint.Image = new Bitmap(history[history.Count - 1 - undoCount]);
+            graphics = Graphics.FromImage(pictureBoxPaint.Image);
+            
+        }
+
+        private void ponowToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (undoCount <= 0) return;
+            
+            undoCount--;
+            pictureBoxPaint.Image = new Bitmap(history[history.Count - 1 - undoCount]);
+            graphics = Graphics.FromImage(pictureBoxPaint.Image);
+            
+        }
+    }
+}
